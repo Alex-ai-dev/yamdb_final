dbf5a586e2fb25e3ba30b92fa653aeeb481e57e3
Alex <96991887+Alex-ai-dev@users.noreply.github.com>
2022-08-15 13:10:46 +0200

    Update serializers.py

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,4 @@
-from django.db.models import Avg   
-
+from django.db.models import Avg
 from django.utils import timezone
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
